c9f2f639bb628604e3f3f6c68496efd6816997c4
Juan Salles <juanprazeres@gmail.com>
2025-02-18 13:14:11 -0300

    create a theme using ThemeProvider and add the default font of the project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,25 +1,53 @@
 'use client'
 
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+	
+	@font-face {
+		font-display: swap; /* Check https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/font-display for other options. */
+		font-family: 'Poppins';
+		font-style: normal;
+		font-weight: 300;
+		src: url('/fonts/poppins-v22-latin-300.woff2') format('woff2'); /* Chrome 36+, Opera 23+, Firefox 39+, Safari 12+, iOS 10+ */
+	}
+
+	
+	@font-face {
+		font-display: swap; /* Check https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/font-display for other options. */
+		font-family: 'Poppins';
+		font-style: normal;
+		font-weight: 400;
+		src: url('/fonts/poppins-v22-latin-regular.woff2') format('woff2'); /* Chrome 36+, Opera 23+, Firefox 39+, Safari 12+, iOS 10+ */
+	}
+
+	
+	@font-face {
+		font-display: swap; /* Check https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/font-display for other options. */
+		font-family: 'Poppins';
+		font-style: normal;
+		font-weight: 600;
+		src: url('/fonts/poppins-v22-latin-600.woff2') format('woff2'); /* Chrome 36+, Opera 23+, Firefox 39+, Safari 12+, iOS 10+ */
+	}
+
   * {
       margin: 0;
 			padding: 0;
 			box-sizing: border-box;
+			-webkit-font-smoothing: antialiased;
+  		-moz-osx-font-smoothing: grayscale;
   }
-	
-	html {
-			font-size: 62.5%;
-	}
-	
-	html, body, #__next {
-			height: 100%;
-	}
 
-	body {
-		font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-	}
+	${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+	
 `
 export default GlobalStyles
